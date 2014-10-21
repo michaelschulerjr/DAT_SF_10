57d59e003665fdec3a46ee9f1fff740dee0b666a
Mike <michaelschulerjr@gmail.com>
2014-10-20 20:30:32 -0700

    changes to lab 2&3

--- Homework/HW1/HW_1.py
+++ Homework/HW1/HW_1.py
@@ -6,7 +6,6 @@ json_data = open("ga_hw_logins.json")
 data = json.load(json_data)
 
 date_object_data = []
-
 for item in data:
 	date_object = datetime.strptime(item, '%Y-%m-%d %H:%M:%S')
 	date_object_data.append(date_object)
